876678c8eb2f96c616887a36ae37c1304e54e936
Vincent Riva <vincent.riva@outlook.fr>
2021-04-09 11:34:05 +0200

    Add filters

--- src/components/Filter.js
+++ src/components/Filter.js
@@ -1,3 +1,14 @@
+import styles from '@/styles/_components/Filter.module.scss';
+import Button from './Button';
+
 export default function Filter() {
-  return <div></div>;
+  return (
+    <div className={styles.container}>
+      <Button style='primary' href='#' icon={require('@/images/icons/filter.svg')} />
+      <input type='text' placeholder='Tags (ex: Android, iPhone, Web, UI, UX)' />
+      <input type='text' placeholder='Colors (ex: enter hex code or select)' />
+      <input type='text' placeholder='Made With' />
+      <input type='text' placeholder='Download' />
+    </div>
+  );
 }
